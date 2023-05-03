9a161d53841bde53822aa27b02cdf8352d9a36dd
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-05-04 01:17:06 +0900

    launch 書き換え

--- launch/myname_pubsub_launch.py
+++ launch/myname_pubsub_launch.py
@@ -2,15 +2,31 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package="myname_pubsub",
-            executable="name_pubsub",
-            name="myname_pubsub_launch",
-            output="screen",
-            emulate_tty=True,
-            parameters=[
-                {"pub_rate": 1.5}
-            ]
-        )
-    ])
+    ld = LaunchDescription()
+
+    talker_comp = Node(
+        package="myname_pubsub",
+        executable="name_pubsub",
+        name="talker_launch",
+        output="screen",
+        emulate_tty=True,
+        parameters=[
+            {"pub_rate": 1.5}
+        ]
+    )
+
+    listener_comp = Node(
+        package="myname_pubsub",
+        executable="name_pubsub",
+        name="listener_launch",
+        output="screen",
+        emulate_tty=True,
+        parameters=[
+            {"pub_rate": 1.5}
+        ]
+    )
+
+    ld.add_action(talker_comp)
+    ld.add_action(listener_comp)
+
+    return ld
