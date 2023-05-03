2ed427d4a5e852290a18c1bc712fd8602bc4bc78
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-05-03 14:31:17 +0900

    update listener node

---
+++ theme1/include/theme1/listener_comp.hpp
@@ -0,0 +1,24 @@
+#ifndef LISTENER_COMP_HPP_
+#define LISTENER_COMP_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+namespace theme1
+{
+
+class ListenerComp : public rclcpp::Node
+{
+public:
+    ListenerComp();
+
+private:
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+
+    void topic_callback(const std_msgs::msg::String &msg) const;
+
+};
+
+} // namespace theme1
+
+#endif // LISTENER_COMP_HPP_
