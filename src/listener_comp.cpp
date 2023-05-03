fc3e1118114e74eff780d0d042d500d2806933cd
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-05-03 17:11:19 +0900

    rename

--- src/listener_comp.cpp
+++ src/listener_comp.cpp
@@ -1,10 +1,10 @@
-#include "theme1/listener_comp.hpp"
+#include "myname_pubsub/listener_comp.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
 #include <memory>
 
 using std::placeholders::_1;
 
-namespace theme1
+namespace myname_pubsub
 {
 
 ListenerComp::ListenerComp(const rclcpp::NodeOptions &options)
@@ -27,14 +27,14 @@ void ListenerComp::topic_callback(const std_msgs::msg::String &msg) const
     RCLCPP_INFO(this->get_logger(), "Subscibed : '%s'", msg.data.c_str());
 }
 
-} // namespace theme1
+} // namespace myname_pubsub
 
-RCLCPP_COMPONENTS_REGISTER_NODE(theme1::ListenerComp)
+RCLCPP_COMPONENTS_REGISTER_NODE(myname_pubsub::ListenerComp)
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<theme1::ListenerComp>());
+    rclcpp::spin(std::make_shared<myname_pubsub::ListenerComp>());
     rclcpp::shutdown();
     return 0;
 }
