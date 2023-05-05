69d9957a3f4041459d8531687275896a8837db5c
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-05-05 17:55:23 +0900

    rename

--- include/myname_pubsub/main.hpp
+++ include/myname_pubsub/main.hpp
@@ -1,11 +1,26 @@
-#ifndef LISTENER_COMP_HPP_
-#define LISTENER_COMP_HPP_
+#ifndef MAIN_HPP_
+#define MAIN_HPP_
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
 namespace myname_pubsub
 {
+
+class TalkerComp : public rclcpp::Node
+{
+public:
+    TalkerComp();
+    explicit TalkerComp(const rclcpp::NodeOptions & options);
+
+private:
+    size_t count_;
+    std::chrono::milliseconds ms_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+
+    void timer_callback();
+};
 
 class ListenerComp : public rclcpp::Node
 {
@@ -19,7 +34,7 @@ private:
     void topic_callback(const std_msgs::msg::String &msg) const;
 
 };
-
+    
 } // namespace myname_pubsub
 
-#endif // LISTENER_COMP_HPP_
+#endif // MAIN_HPP_
