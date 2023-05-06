a8f66168b8131b284f84adf60a75fc336706b710
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-05-06 18:49:11 +0900

    Code format on ament_uncrustify for header

--- include/myname_pubsub/main.hpp
+++ include/myname_pubsub/main.hpp
@@ -25,18 +25,18 @@ namespace myname_pubsub
 class Myname : public rclcpp::Node
 {
 public:
-    explicit Myname(const rclcpp::NodeOptions & options);
+  explicit Myname(const rclcpp::NodeOptions & options);
 
 private:
-    size_t count_;
-    std::string name_;
-    std::chrono::milliseconds ms_;
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  size_t count_;
+  std::string name_;
+  std::chrono::milliseconds ms_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 
-    void timer_callback();
-    void topic_callback(const std_msgs::msg::String &msg) const;
+  void timer_callback();
+  void topic_callback(const std_msgs::msg::String & msg) const;
 
 };
 
